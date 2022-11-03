b61f63821017c441763a4cbcad8c5a614806a35a
Quillraven <quillraven@gmail.com>
2022-11-03 20:07:39 +0100

    repack UI graphics

--- core/src/main/kotlin/com/github/quillraven/mysticwoods/ui/skin.kt
+++ core/src/main/kotlin/com/github/quillraven/mysticwoods/ui/skin.kt
@@ -2,10 +2,8 @@ package com.github.quillraven.mysticwoods.ui
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
-import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable
 import ktx.assets.disposeSafely
@@ -25,7 +23,7 @@ enum class Drawables(
     FRAME_BGD("frame_bgd"),
     FRAME_FGD("frame_fgd"),
     INVENTORY_SLOT("inv_slot"),
-    INVENTORY_SLOT_HELMET("inv_slot_helmer"),
+    INVENTORY_SLOT_HELMET("inv_slot_helmet"),
     INVENTORY_SLOT_ARMOR("inv_slot_armor"),
     INVENTORY_SLOT_WEAPON("inv_slot_weapon"),
     INVENTORY_SLOT_BOOTS("inv_slot_boots"),
@@ -54,20 +52,7 @@ enum class Labels {
 }
 
 fun loadSkin() {
-    val atlas = TextureAtlas("ui/ui.atlas")
-    // TODO remove this line when invslot graphic is part of atlas
-    atlas.addRegion(Drawables.INVENTORY_SLOT.atlasKey, TextureRegion(Texture("ui/inv_slot.png")))
-    atlas.addRegion(Drawables.INVENTORY_SLOT_HELMET.atlasKey, TextureRegion(Texture("ui/inv_slot_helmet.png")))
-    atlas.addRegion(Drawables.INVENTORY_SLOT_WEAPON.atlasKey, TextureRegion(Texture("ui/inv_slot_weapon.png")))
-    atlas.addRegion(Drawables.INVENTORY_SLOT_ARMOR.atlasKey, TextureRegion(Texture("ui/inv_slot_armor.png")))
-    atlas.addRegion(Drawables.INVENTORY_SLOT_BOOTS.atlasKey, TextureRegion(Texture("ui/inv_slot_boots.png")))
-    atlas.addRegion("helmet", TextureRegion(Texture("ui/helmet.png")))
-    atlas.addRegion("armor", TextureRegion(Texture("ui/armor.png")))
-    atlas.addRegion("sword", TextureRegion(Texture("ui/sword.png")))
-    atlas.addRegion("sword2", TextureRegion(Texture("ui/sword2.png")))
-    atlas.addRegion("boots", TextureRegion(Texture("ui/boots.png")))
-
-    Scene2DSkin.defaultSkin = skin(atlas) { skin ->
+    Scene2DSkin.defaultSkin = skin(TextureAtlas("ui/ui.atlas")) { skin ->
         Fonts.values().forEach { fnt ->
             skin[fnt.skinKey] =
                 BitmapFont(Gdx.files.internal(fnt.fontPath), skin.getRegion(fnt.atlasRegionKey)).apply {
